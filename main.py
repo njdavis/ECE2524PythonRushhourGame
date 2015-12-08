b76b0c061a85e6719355bf776b216128153dc134
Noah Davis <noah_davis@outlook.com>
2015-12-08 15:43:00 -0500

    Simplified main

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-
+#Main file
+#created by Noah and Pia
 
 import board
+import difficulty
 
-#Initialize the gameboard with blank spaces
-pos  = ['  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ']
-
-print ("Do you wish to play easy, medium, or hard?")
-print ("Type 'easy', 'medium', or 'hard' and press enter")
-difficulty = input()
-
-print ( difficulty)
-
-#car 0
-pos[15] = '00'
-pos[16] = '00'
-
-#car 1
-pos[5] = '11'
-pos[11] = '11'
-pos[17] = '11'
-
-#car 2
-pos[28] = '22'
-pos[29] = '22'
-
-#car 3 
-pos[27] = '33'
-pos[33] = '33'
+pos = difficulty.decideDiff() 
 
 board.printBoard(pos) 
