90c9e53743d22e33d86d122d93270a51ab691264
Noah Davis <noah_davis@outlook.com>
2015-12-08 16:25:23 -0500

    wrote function to decode user

---
+++ cars.py
@@ -0,0 +1,82 @@
+#this documents containing functions relating to the movement of game pieces
+#created by Noah Davis
+
+
+def readUser(ans):
+    if ans == 'Aa'
+        return 0
+    elif ans == 'Ab'
+        return 1
+    elif ans == 'Ac'
+        return 2
+    elif ans == 'Ad'
+        return 3
+    elif ans == 'Ae'
+        return 4
+    elif ans == 'Af'
+        return 5
+    elif ans == 'Ba'
+        return 6
+    elif ans == 'Bb'
+        return 7
+    elif ans == 'Bc'
+        return 8
+    elif ans == 'Bd'
+        return 9
+    elif ans == 'Be'
+        return 10
+    elif ans == 'Bf'
+        return 11
+    elif ans == 'Ca'
+        return 12
+    elif ans == 'Cb'
+        return 13
+    elif ans == 'Cc'
+        return 14
+    elif ans == 'Cd'
+        return 15
+    elif ans == 'Ce'
+        return 16
+    elif ans == 'Cf'
+        return 17
+    elif ans == 'Da'
+        return 18
+    elif ans == 'Db'
+        return 19
+    elif ans == 'Dc'
+        return 20
+    elif ans == 'Dd'
+        return 21
+    elif ans == 'De'
+        return 22
+    elif ans == 'Df'
+        return 23
+    elif ans == 'Ea'
+        return 24
+    elif ans == 'Eb'
+        return 25
+    elif ans == 'Ec'
+        return 26
+    elif ans == 'Ed'
+        return 27
+    elif ans == 'Ee'
+        return 28
+    elif ans == 'Ef'
+        return 29
+    elif ans == 'Fa'
+        return 30
+    elif ans == 'Fb'
+        return 31
+    elif ans == 'Fc'
+        return 32
+    elif ans == 'Fd'
+        return 33
+    elif ans == 'Fe'
+        return 34
+    elif ans == 'Ff'
+        return 35
+    else 
+        print ("You enterd the wrong command")
+        return -1
+
+
