f0b41806b02b42619db40c04e2a2ac104fcff395
Noah Davis <noah_davis@outlook.com>
2015-12-08 15:13:06 -0500

    Made a printboard function that can be called from the main file

--- board.py
+++ board.py
@@ -4,39 +4,18 @@
 #This file is meant to print out the gameboard.
 #
 
-
-#carPositionList
-pos  = ['  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ']
-
-#car 0
-pos[15] = '00'
-pos[16] = '00'
-
-#car 1
-pos[5] = '11'
-pos[11] = '11'
-pos[17] = '11'
-
-#car 2
-pos[28] = '22'
-pos[29] = '22'
-
-#car 3 
-pos[27] = '33'
-pos[33] = '33'
-
-
-print ("    A  B  C  D  E  F")
-print ("   -------------------")
-print (" a |%s|%s|%s|%s|%s|%s|" % (pos[0],pos[1],pos[2],pos[3],pos[4],pos[5]) )
-print ("   -------------------")
-print (" b |%s|%s|%s|%s|%s|%s|" % (pos[6],pos[7],pos[8],pos[9],pos[10],pos[11]) )
-print ("   ----------------------")
-print (" c |%s|%s|%s|%s|%s|%s  Exit" % (pos[12],pos[13],pos[14],pos[15],pos[16],pos[17]) )
-print ("   ----------------------")
-print (" d |%s|%s|%s|%s|%s|%s|" % (pos[18],pos[19],pos[20],pos[21],pos[22],pos[23]) )
-print ("   -------------------")
-print (" e |%s|%s|%s|%s|%s|%s|" % (pos[24],pos[25],pos[26],pos[27],pos[28],pos[29]) )
-print ("   -------------------")
-print (" f |%s|%s|%s|%s|%s|%s|" % (pos[30],pos[32],pos[32],pos[33],pos[34],pos[35]) )
-print ("   -------------------")
+def printBoard(pos):
+    print ("    A  B  C  D  E  F")
+    print ("   -------------------")
+    print (" a |%s|%s|%s|%s|%s|%s|" % (pos[0],pos[1],pos[2],pos[3],pos[4],pos[5]) )
+    print ("   -------------------")
+    print (" b |%s|%s|%s|%s|%s|%s|" % (pos[6],pos[7],pos[8],pos[9],pos[10],pos[11]) )
+    print ("   ----------------------")
+    print (" c |%s|%s|%s|%s|%s|%s  Exit" % (pos[12],pos[13],pos[14],pos[15],pos[16],pos[17]) )
+    print ("   ----------------------")
+    print (" d |%s|%s|%s|%s|%s|%s|" % (pos[18],pos[19],pos[20],pos[21],pos[22],pos[23]) )
+    print ("   -------------------")
+    print (" e |%s|%s|%s|%s|%s|%s|" % (pos[24],pos[25],pos[26],pos[27],pos[28],pos[29]) )
+    print ("   -------------------")
+    print (" f |%s|%s|%s|%s|%s|%s|" % (pos[30],pos[32],pos[32],pos[33],pos[34],pos[35]) )
+    print ("   -------------------")
