34592dc748e9a97a6dd236a2a95ae86fe2bdc1b6
Noah Davis <noah_davis@outlook.com>
2015-12-07 22:40:54 -0500

    Created a function to output the board. It currently outputs an empty board

---
+++ board.py
@@ -0,0 +1,26 @@
+#board.py
+#Created by: Noah Davis
+#
+#This file is meant to print out the gameboard.
+#
+
+
+#carPositionList
+cars  = ['  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ','  ']
+
+
+print "    A  B  C  D  E  F"
+print "   -------------------"
+print " a |%d|%d|%d|%d|%d|%d|" % (cars[0],cars[1],cars[2],cars[3],cars[4],cars[5])
+print "   -------------------"
+print " b |%d|%d|%d|%d|%d|%d|" % (cars[6],cars[7],cars[8],cars[9],cars[10],cars[11])
+print "   ----------------------"
+print " c |%d|%d|%d|%d|%d|%d  Exit" % (cars[12],cars[13],cars[14],cars[15],cars[16],cars[17])
+print "   ----------------------"
+print " d |%d|%d|%d|%d|%d|%d|" % (cars[18],cars[19],cars[20],cars[21],cars[22],cars[23])
+print "   -------------------"
+print " e |%d|%d|%d|%d|%d|%d|" % (cars[24],cars[25],cars[26],cars[27],cars[28],cars[29])
+print "   -------------------"
+print " f |%d|%d|%d|%d|%d|%d|" % (cars[30],cars[32],cars[32],cars[33],cars[34],cars[35])
+print "   -------------------"
+
