e15f3e90a4bb78b6319355e30ae0841de28a80a4
pbaner9 <apbaner9@vt.edu>
2015-12-09 19:08:57 -0500

    Added move functions

--- cars.py
+++ cars.py
@@ -1,7 +1,26 @@
 #this documents containing functions relating to the movement of game pieces
-#created by Noah Davis
+#created by Noah Davis & Pia Banerjee
 
 #this function translates user inputs into machine readable variables
+def move(endpos, piece, pos, direction)
+
+        distance = posDistance(piece, pos)
+        endEndPos = endpos + distance
+        #put old values into grid
+        for i in range (0,35):
+                if pos[i] == piece:
+                        pos[i]= '  '
+
+        #add new values into grid
+        if direction == 'vertical':
+                for i range (endpos, endEndPos, 6):
+        elif direction == 'horizontal':
+                for i range (endpos, endEndPos):
+        else:
+                print "Something is wrong"
+
+        return pos
+
 def readUser(ans):
     if ans == 'Aa':
         return 0
@@ -107,5 +126,30 @@ def carDirection(piece, pos):
     else :
         return 'Vertical'
 
+
+#checking position for moving piece into 
+
+def CorrectMove (endpos, piece, pos)
+        move position = cars.readUser(endpos)
+
+        for i in range (0,35):
+                if piece == pos[i]:
+                        startpos = i
+
+        direction = board.direction (pos[startpos])
+
+        if direction == 'horizontal':
+                for i in range (startpos, endpos):
+                        if (pos[i] != ' ') || (pos[i] != piece:
+                                print "Can't move piece here!"
+                        else:
+                                output = move(endpos, piece, pos,direction)
+        elif direction == 'vertical':
+                for i in range (startpos, endpos,6):
+                        if (pos[i] != ' ') || (pos[i] != piece:
+                                print "Can't move piece here!"
+                        else:
+                                output= move(endpos, piece, pos, direction)
+        return output
 
 
