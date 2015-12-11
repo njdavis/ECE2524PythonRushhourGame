d0174943eba0b32ddd7b1af75807f8c2e4acbefd
njdavis <noah_davis@outlook.com>
2015-12-11 18:48:05 -0500

    Update cars.py

--- cars.py
+++ cars.py
@@ -24,73 +24,73 @@ def move(endpos, piece, pos, direction)
 def readUser(ans):
     if ans == 'Aa':
         return 0
-    elif ans == 'Ab':
-        return 1
-    elif ans == 'Ac':
-        return 2
-    elif ans == 'Ad':
-        return 3
-    elif ans == 'Ae':
-        return 4
-    elif ans == 'Af':
-        return 5
     elif ans == 'Ba':
+        return 1
+    elif ans == 'Ca':
+        return 2
+    elif ans == 'Da':
+        return 3
+    elif ans == 'Ea':
+        return 4
+    elif ans == 'Fa':
+        return 5
+    elif ans == 'Ab':
         return 6
     elif ans == 'Bb':
         return 7
-    elif ans == 'Bc':
-        return 8
-    elif ans == 'Bd':
-        return 9
-    elif ans == 'Be':
-        return 10
-    elif ans == 'Bf':
-        return 11
-    elif ans == 'Ca':
-        return 12
     elif ans == 'Cb':
+        return 8
+    elif ans == 'Db':
+        return 9
+    elif ans == 'Eb':
+        return 10
+    elif ans == 'Fb':
+        return 11
+    elif ans == 'Ac':
+        return 12
+    elif ans == 'Bc':
         return 13
     elif ans == 'Cc':
         return 14
-    elif ans == 'Cd':
-        return 15
-    elif ans == 'Ce':
-        return 16
-    elif ans == 'Cf':
-        return 17
-    elif ans == 'Da':
-        return 18
-    elif ans == 'Db':
-        return 19
     elif ans == 'Dc':
+        return 15
+    elif ans == 'Ec':
+        return 16
+    elif ans == 'Fc':
+        return 17
+    elif ans == 'Ad':
+        return 18
+    elif ans == 'Bd':
+        return 19
+    elif ans == 'Cd':
         return 20
     elif ans == 'Dd':
         return 21
-    elif ans == 'De':
-        return 22
-    elif ans == 'Df':
-        return 23
-    elif ans == 'Ea':
-        return 24
-    elif ans == 'Eb':
-        return 25
-    elif ans == 'Ec':
-        return 26
     elif ans == 'Ed':
+        return 22
+    elif ans == 'Fd':
+        return 23
+    elif ans == 'Ae':
+        return 24
+    elif ans == 'Be':
+        return 25
+    elif ans == 'Ce':
+        return 26
+    elif ans == 'De':
         return 27
     elif ans == 'Ee':
         return 28
     elif ans == 'Ef':
         return 29
-    elif ans == 'Fa':
+    elif ans == 'Af':
         return 30
-    elif ans == 'Fb':
+    elif ans == 'Bf':
         return 31
-    elif ans == 'Fc':
+    elif ans == 'Cf':
         return 32
-    elif ans == 'Fd':
+    elif ans == 'Df':
         return 33
-    elif ans == 'Fe':
+    elif ans == 'Ef':
         return 34
     elif ans == 'Ff':
         return 35
