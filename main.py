4119aa5b9fd3ab8f61f0851aa9e6c06ce7ac97e3
pbaner9 <apbaner9@vt.edu>
2015-12-11 20:12:17 -0500

    Added comments within main for game-win situations

--- main.py
+++ main.py
@@ -5,22 +5,28 @@ import board
 
 youWin = False
 
+#START OF GAME
+
+#Display of gameboard 
 gameBoard = board.decideDiff() 
 
 board.printBoard(gameBoard) 
+
 
 while youWin == False:
     piece = input("Please enter the car you would like to move (ex: 11): ")
     endpos = input("Please enter the end position (of the top most piece or left most piece) piece of the car (ex: Aa): ")
 
+    #Placing appropriate variables into pre-defined functions
     endpos = cars.readUser(endpos)
     direction = cars.carDirection(piece, gameBoard)
 
     gameBoard = cars.correctMove(endpos, piece, gameBoard)
     board.printBoard(gameBoard)
 
+#win condition
     if gameBoard[17] == '00':
         youWin = True
-
+#Jackie Chan display when win
 if youWin ==  True:
-    board.printJackieChan()
+    boardc.printJackieChan()
