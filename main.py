042dfc37ab0d790d425ec4b4c18128cfe0805f6b
Noah Davis <noah_davis@outlook.com>
2015-12-11 19:07:15 -0500

    Changed main file to ask for user input

--- main.py
+++ main.py
@@ -5,14 +5,22 @@ import board
 
 youWin = False
 
-pos = board.decideDiff() 
-length = cars.posDistance('22',pos)
-direction = cars.carDirection('22', pos)
+gameBoard = board.decideDiff() 
 
-print (length)
-print (direction)
+board.printBoard(gameBoard) 
 
-board.printBoard(pos) 
+while youWin == False:
+    piece = input("Please enter the car you would like to move (ex: 11): ")
+    endpos = input("Please enter the end position (of the top most piece or right most piece) piece of the car (ex: Aa): ")
+
+    endpos = cars.readUser(endpos)
+    direction = cars.carDirection(piece, gameBoard)
+
+    gameBoard = cars.correctMove(endpos, piece, gameBoard)
+    board.printBoard(gameBoard)
+
+    if gameBoard[17] == '00':
+        youWin = True
 
 if youWin ==  True:
     board.printJackieChan()
